0d9e2bc51ceb145ce93dec850ca16c335befd273
berezhok <berezhok1988@gmail.com>
2024-05-14 16:47:36 +0300

    final

--- 01/src/main.tf
+++ 01/src/main.tf
@@ -1,14 +1,18 @@
+
 terraform {
   required_providers {
+    # We recommend pinning to the specific version of the Docker Provider you're using
+    # since new versions are released frequently
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
+      version = "3.0.2"
     }
   }
-  required_version = ">=0.13" /*Многострочный комментарий.
- Требуемая версия terraform */
 }
-provider "docker" {}
+
+# Configure the docker provider
+provider "docker" {
+}
 
 #однострочный комментарий
 
@@ -20,19 +24,17 @@ resource "random_password" "random_string" {
   min_numeric = 1
 }
 
-/*
-resource "docker_image" {
+resource "docker_image" "nginx" {
   name         = "nginx:latest"
   keep_locally = true
 }
 
-resource "docker_container" "1nginx" {
+resource "docker_container" "hello_world" {
   image = docker_image.nginx.image_id
-  name  = "example_${random_password.random_string_FAKE.resulT}"
+  name  = "hello_world"
 
   ports {
     internal = 80
     external = 9090
   }
 }
-*/
